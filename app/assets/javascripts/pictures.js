2fa0103266b0ef87e64c01596482358a04a6f000
EC2 Default User <ec2-user@ip-172-31-11-171.us-east-2.compute.internal>
2018-10-05 06:00:34 +0000

    First commit succeed to takepicture Add: download, downloads, ajax with scaffold

---
+++ app/assets/javascripts/pictures.js
@@ -0,0 +1,120 @@
+$(function() {
+	//videoタグを取得
+	var video = document.getElementById('camera');
+	//カメラが起動できたかのフラグ
+	var localMediaStream = null;
+	//カメラ使えるかチェック
+	var hasGetUserMedia = function() {
+		return (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	};
+
+	//エラー
+	var onFailSoHard = function(e) {
+		console.log('エラー!', e);
+	};
+
+	if(!hasGetUserMedia()) {
+		alert("未対応ブラウザです。");
+	} else {
+		window.URL = window.URL || window.webkitURL;
+		navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		navigator.getUserMedia({video: true}, function(stream) {
+			video.src = window.URL.createObjectURL(stream);
+			localMediaStream = stream;
+			video.width = 400;
+			video.height = 300;
+		}, onFailSoHard);
+	}
+
+	$("#start").click(function() {
+		if (localMediaStream) {
+			var canvas = document.getElementById('canvas');
+			//canvasの描画モードを2sに
+			var ctx = canvas.getContext('2d');
+			var img = document.getElementById('img');
+
+			//videoの縦幅横幅を取得
+			video.width = 400
+			var w = video.width;
+			video.height = 300
+			var h = video.height;
+
+			//同じサイズをcanvasに指定
+			canvas.setAttribute("width", w);
+			canvas.setAttribute("height", h);
+
+			//canvasにコピー
+			ctx.drawImage(video, 0, 0, w, h);
+			//imgにpng形式で書き出し
+			img.src = canvas.toDataURL('image/jpeg');
+			
+		}
+	});
+	$("#actions").click(function(){
+		if (localMediaStream) {
+			var canvas = document.getElementById('canvas');
+			//canvasの描画モードを2sに
+			var ctx = canvas.getContext('2d');
+			var img = document.getElementById('img');
+
+			//videoの縦幅横幅を取得
+			var w = video.offsetWidth;
+			var h = video.offsetHeight;
+
+			//同じサイズをcanvasに指定
+			canvas.setAttribute("width", w);
+			canvas.setAttribute("height", h);
+
+			//canvasにコピー
+			ctx.drawImage(video, 0, 0, w, h);
+			//imgにpng形式で書き出し
+			img.src = canvas.toDataURL('image/png');
+			
+		}
+	});
+	$('#save-button').click(function(){
+		var canvas = document.getElementById('canvas');
+		
+		var url = canvas.toDataURL('image/jpeg');
+	    
+		$("#picture_pic").val(""); 
+		$("#picture_pic").val(url);
+		$("#new_picture").submit();
+	});
+});
+
+		
+
+// Base64データをBlobデータに変換
+function Base64toBlob(base64)
+{
+    // カンマで分割して以下のようにデータを分ける
+    // tmp[0] : データ形式（data:image/png;base64）
+    // tmp[1] : base64データ（iVBORw0k～）
+    var tmp = base64.split(',');
+    // base64データの文字列をデコード
+    var data = atob(tmp[1]);
+    // tmp[0]の文字列（data:image/png;base64）からコンテンツタイプ（image/png）部分を取得
+		var mime = tmp[0].split(':')[1].split(';')[0];
+	    //  1文字ごとにUTF-16コードを表す 0から65535 の整数を取得
+		var buf = new Uint8Array(data.length);
+		for (var i = 0; i < data.length; i++) {
+	        buf[i] = data.charCodeAt(i);
+	    }
+	    // blobデータを作成
+		var blob = new Blob([buf], { type: mime });
+	    return blob;
+}		
+
+function dataURLtoBlob(dataURL) {
+	  // Decode the dataURL
+	  var binary = atob(dataURL.split(',')[1]);
+	  // Create 8-bit unsigned array
+	  var array = [];
+	  for(var i = 0; i < binary.length; i++) {
+	      array.push(binary.charCodeAt(i));
+	  }
+	  // Return our Blob object
+	  blob = new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+	  return blob;
+	}
---
+++ app/controllers/indices_controller.rb
@@ -0,0 +1,106 @@
+class IndicesController < ApplicationController
+  require 'rubyzip.rb'
+  before_action :set_index, only: [:show, :edit, :update, :destroy]
+
+  # GET /indices
+  # GET /indices.json
+  def index
+    @indices = Index.includes(:pictures).all
+  end
+
+  # GET /indices/1
+  # GET /indices/1.json
+  def show
+  end
+  
+  # GET /indices/download
+  def downloads
+    # downloadsのサブディレクトリだけを取り出し
+  #   Dir.glob("#{Rails.root}/public/downloads/**").each{ |name|
+		# if FileTest.directory?(name)
+			
+  #   end
+  #   }
+    directory_to_zip = "#{Rails.root}/public/downloads"
+    output_file = "#{Rails.root}/lib/downloads.zip"
+    zip_file_generator = ZipFileGenerator.new(directory_to_zip, output_file)
+    # binding.pry
+    zip_file_generator.write
+    send_file(output_file, filename: 'image.zip', disposition: 'attachment', stream: true)
+    
+  end
+  
+  def download
+    # binding.pry
+    @index = params['id']
+    directory_to_zip = "#{Rails.root}/public/downloads/name_no#{@index}"
+    output_file = "#{Rails.root}/public/downloads/name_no#{@index}.zip"
+    zip_file_generator = ZipFileGenerator.new(directory_to_zip, output_file)
+    zip_file_generator.write
+    # if FileTest.directory?(zip_file_generator)
+      send_file(output_file, filename: "name_no#{@index}.zip", disposition: 'attachment', stream: true)
+    # else
+    #   alert(画像がありません)
+    # end
+  end
+
+  # GET /indices/new
+  def new
+    @index = Index.new
+  end
+
+  # GET /indices/1/edit
+  def edit
+  end
+
+  # POST /indices
+  # POST /indices.json
+  def create
+    @index = Index.new(index_params)
+
+    respond_to do |format|
+      if @index.save
+        format.html { redirect_to @index, notice: 'Index was successfully created.' }
+        format.json { render :show, status: :created, location: @index }
+      else
+        format.html { render :new }
+        format.json { render json: @index.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /indices/1
+  # PATCH/PUT /indices/1.json
+  def update
+    respond_to do |format|
+      if @index.update(index_params)
+        format.html { redirect_to @index, notice: 'Index was successfully updated.' }
+        format.json { render :show, status: :ok, location: @index }
+      else
+        format.html { render :edit }
+        format.json { render json: @index.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /indices/1
+  # DELETE /indices/1.json
+  def destroy
+    @index.destroy
+    respond_to do |format|
+      format.html { redirect_to indices_url, notice: 'Index was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_index
+      @index = Index.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def index_params
+      params.require(:index).permit(:name, :num, :pictures_count)
+    end
+end
