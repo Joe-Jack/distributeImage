b614ce8fa1c49104832b4854eead7bdd70c766ce
EC2 Default User <ec2-user@ip-172-31-11-171.us-east-2.compute.internal>
2018-12-06 10:51:46 +0000

    upload to heroku, add actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer.default_url_options = {protocol: 'https', host: 'distributeimage.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "smtp.gmail.com",
+    :user_name => Rails.application.secrets.google_address,
+    :password => Rails.application.secrets.google_password,
+    :enable_starttls_auto => true,
+    :authentication => "plain",
+    }
   
 end
