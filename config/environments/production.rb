4e8d3070480c7afb8e9c0920d3215ce3ff80549e
EC2 Default User <ec2-user@ip-172-31-11-171.us-east-2.compute.internal>
2018-12-06 11:46:02 +0000

    upload to heroku, herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,17 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = {host: 'distributeimage.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: "distributeimage.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "smtp.gmail.com",
+    :domain => "herokuapp.com",
     :user_name => Rails.application.secrets.google_address,
     :password => Rails.application.secrets.google_password,
     :enable_starttls_auto => true,
-    :authentication => "login",
+    :authentication => "login"
     }
   
 end
